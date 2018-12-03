f04416b098fa460d8b334e7e1f06f43a45ecdd04
754995643 <754995643@qq.com>
2018-12-03 11:03:53 +0800

    修改权限申请。

--- app/src/main/java/com/spd/pk30dome/ui/DeviceScanActivity.java
+++ app/src/main/java/com/spd/pk30dome/ui/DeviceScanActivity.java
@@ -244,16 +244,16 @@ public class DeviceScanActivity extends ListActivity {
             } else {
                 viewHolder = (ViewHolder) view.getTag();
             }
-
-            BluetoothDevice device = mLeDevices.get(i);
-            final String deviceName = device.getName();
-            if (deviceName != null && deviceName.length() > 0) {
-                viewHolder.deviceName.setText(deviceName);
-            } else {
-                viewHolder.deviceName.setText(R.string.unknown_device);
+            if (mLeDevices.size() > i) {
+                BluetoothDevice device = mLeDevices.get(i);
+                final String deviceName = device.getName();
+                if (deviceName != null && deviceName.length() > 0) {
+                    viewHolder.deviceName.setText(deviceName);
+                } else {
+                    viewHolder.deviceName.setText(R.string.unknown_device);
+                }
+                viewHolder.deviceAddress.setText(device.getAddress());
             }
-            viewHolder.deviceAddress.setText(device.getAddress());
-
             return view;
         }
     }
